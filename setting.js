14b4ee013c5a471df0bdc907bb9d8f2c28ce7a17
Ghazidon39 <unknownkiller276@gmail.com>
2026-02-09 06:53:33 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '⎯᪵⎯꯭᪳𝆺꯭𝆭🔱꯭𝐌ı֟፝݊ɼʑ፝֟ɑ  Ӿ  𝐆ⱶ֟ؖ۬꧊ɑ͢ʑ፝֟ı˺ ⃪꯭꯭🇦🇱𝆺𝅥𝆬',
+  author: '‎',
+  botName: "⎯᪵⎯꯭᪳𝆺꯭𝆭🔱꯭𝐌ı֟፝݊ɼʑ፝֟ɑ  Ӿ  𝐆ⱶ֟ؖ۬꧊ɑ͢ʑ፝֟ı˺ ⃪꯭꯭🇦🇱𝆺𝅥𝆬",
+  botOwner: 'GHAZI-X-MIRZA', // Your name
+  ownerNumber: '923706289711', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/Ghazidon39/MIRZA-X-GHAZI-MD-BOT",
+};
+
+module.exports = settings;
