22c7c6302877f921137a584e1905c30d44fd220f
Ghazidon39 <unknownkiller276@gmail.com>
2026-02-11 06:59:59 +0500

    Update newsletter JID in contextInfo

--- main.js
+++ main.js
@@ -155,7 +155,7 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363279142099991@newsletter',
+            newsletterJid: '120363425065986597@newsletter',
             newsletterName: 'MIRZA-X-LEGEND',
             serverMessageId: -1
         }
